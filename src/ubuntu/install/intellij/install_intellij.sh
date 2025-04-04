0f9ba7e6d6352a249c8d12e205fa64a002afe44a
Devin Catuns <123214008+dcat23@users.noreply.github.com>
2025-04-04 03:55:21 +0000

    fix: set desktop output file for intelliJ

--- src/ubuntu/install/intellij/install_intellij.sh
+++ src/ubuntu/install/intellij/install_intellij.sh
@@ -4,7 +4,9 @@ INTELLIJ_VER_DATE="2024.3.5"
 
 cd /tmp
 wget -q -O intellij.tar.gz "https://download.jetbrains.com/idea/ideaIU-${INTELLIJ_VER_DATE}.tar.gz"
-tar -xzf intellij.tar.gz -C /opt
+tar -xzf intellij.tar.gz --one-top-level=/opt/idea-IU --strip-components=1
 
 cp $INST_SCRIPTS/intellij/intellij.desktop $HOME/Desktop/
 cp $INST_SCRIPTS/intellij/intellij.desktop /usr/share/applications/
+
+chmod +x $HOME/Desktop/intellij.desktop /usr/share/applications/intellij.desktop
