cc461f54428ed0e4190d6c0e4a01613f02086fb2
Devin Catuns <devincatuns1@gmail.com>
2025-04-04 18:01:04 -0500

    feat: add open ssh- upate OpenJDK to 21 (java-dev)

--- src/ubuntu/install/intellij/install_intellij.sh
+++ src/ubuntu/install/intellij/install_intellij.sh
@@ -4,9 +4,11 @@ INTELLIJ_VER_DATE="2024.3.5"
 
 cd /tmp
 wget -q -O intellij.tar.gz "https://download.jetbrains.com/idea/ideaIU-${INTELLIJ_VER_DATE}.tar.gz"
-tar -xzf intellij.tar.gz --one-top-level=/opt/idea-IU --strip-components=1
+tar -xzf intellij.tar.gz --one-top-level=/opt/idea-IU --strip-components=1 \
+    && rm intellij.tar.gz
 
 cp $INST_SCRIPTS/intellij/intellij.desktop $HOME/Desktop/
 cp $INST_SCRIPTS/intellij/intellij.desktop /usr/share/applications/
 
-chmod +x $HOME/Desktop/intellij.desktop /usr/share/applications/intellij.desktop
+chmod +x $HOME/Desktop/intellij.desktop 
+chmod +x /usr/share/applications/intellij.desktop
